2289d14cf0061ff82f94dbcb6b6792953f58a654
dgobster <gobster@wisc.edu>
2023-04-23 12:10:27 -0500

    Leaflet base

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-//insert code here!
+//declare global variables
+var map;
+
+//function to instantiate the Leaflet map
+function createMap() {
+    //create the map
+    map = L.map('map', {
+        center: [43.416993, -89.948362
+        ],
+        zoom: 8
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        opacity: .65,
+        attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    //call getData function
+    getData(map);
+};
+
